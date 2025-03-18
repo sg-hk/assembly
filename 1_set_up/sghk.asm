2d3f9a732fa9f91d9657c89eef711c6f81ccd895
sg-hk <rubenruben000@proton.me>
2025-03-19 06:48:27 +0800

    second challenge

--- 1_set_up/sghk.asm
+++ 1_set_up/sghk.asm
@@ -1,25 +1,13 @@
-;; first try:
-;; I had initially simply written
-;; global _start
-;; and tried to compile with "nasm sghk.asm"
-;; this produced an empty file
-
 .globl _start    
 
 .text           
 _start:
-    mov x16, #1    
-    mov x0, #0      
+    mov x16, #1 ;; exit syscall (1)
+    mov x0, #0 ;; exit status = exit(0)
     svc #0x80      
 
-
-;; lessons:
-;; 1. code
-;; global _start is the entry point
-;; _start: then needs to be defined
-;; to simply exit you still need to load up a register and syscall
-;; 2. compilation
-;; the correct command is
+;; compilation commands
+;; x86 linux
 ;; nasm -f elf64 file.asm -o file.o && ld file.o -o file
-;; on macOS:
-;; clang -c file.ask -o file && ld file.o -o sghk -lSystem -syslibroot `xcrun --show-sdk-path` -e _start
+;; macOS
+;; clang -c file.ask -o file.o && ld file.o -o file -lSystem -syslibroot `xcrun --show-sdk-path` -e _start
