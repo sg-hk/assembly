48414b16eafc265e127a57e5a9ae47348590dd27
sg-hk <rubenruben000@proton.me>
2025-03-23 11:07:33 +0800

    fix macro branching

--- 5_stack/sghk.asm
+++ 5_stack/sghk.asm
@@ -6,79 +6,67 @@ _res_str:     .space 64
 _first_n:     .long 1
 _second_n:    .long 2
 _third_n:     .long 3
-_array_n:     .space 16
-_res_str:     .space 64
-_new_line:    .byte 10 ; "\n" in ASCII
+_newline:     .byte 10 ; "\n" in ASCII
 
 .section __TEXT, __text
-.macro _PUSH _arr, _num
-  adrp  x0, _arr@PAGE
-  add   x0, x0, _arr@PAGEOFF
-  adrp  x1, _num@PAGE
-  add   x1, x1, _num@PAGEOFF
-  str   x1, [x0]
-.endm
-.macro _POP _arr, _num
-  adrp  x0, _arr@PAGE
-  add   x0, x0, _arr@PAGEOFF
-  adrp  x1, _num@PAGE
-  add   x1, x1, _num@PAGEOFF
-.endm
 .macro _PRINT_INT _number, _string
-  adrp  w0, _number@PAGE
-  add   w0, w0, _number@PAGEOFF
-  adrp	x12, _res_str@PAGE
-  add	x12, x12, _res_str@PAGEOFF 
-  mov	w13, #10          ; the base (10)
-itoa:
-  ; we first get the last digit
-  udiv	w14, w0, w13      ; res/10 (no decimals)
-  mul	w15, w14, w13     ; (res/10)*10, eg 333 becomes 330
-  sub	w16, w0, w15      ; res - (res/10)*10, eg 333-330 = last digit
-  ; then we convert to ASCII
-  add	w16, w16, #48     ; add 48 = ASCII value for digit 
-  strb	w16, [x12], #1    ; store 1 byte (ASCII variable) in string
-  ; and increment string: point to next char
-  mov	w0, w14           ; copy the quotient (res/10 no dec.) in res
-  cbnz	w0, itoa          ; if quotient != 0, continue
-  ; cbnz is an optimization of cmp, bne
+  adrp    x2, \_number@PAGE
+  add     x2, x2, \_number@PAGEOFF
+  ldr     w0, [x2]
 
-  ; then we do strlen
-  adrp	x17, _res_str@PAGE
-  add	x17, x17, _res_str@PAGEOFF ; x17 points to start of str
-  mov	x20, x12          ; x12 points to end of str (after last char)
-  sub	x20, x20, x17     ; end-start = len
+  adrp    x12, \_string@PAGE
+  add     x12, x12, \_string@PAGEOFF
 
-  ; now reverse
-  sub	x12, x12, #1      ; we decrement to point at last char
-rev:
-  cmp	x17, x12
-  b.ge	print             ; go to print when at string start
-  ldrb	w19, [x17]        ; load byte at start
-  ldrb	w21, [x12]        ; load byte at end
-  strb	w21, [x17]        ; swap bytes
-  strb	w19, [x12]
-  add	x17, x17, #1      ; increment start, decrement end
-  sub	x12, x12, #1
-  b	rev               ; unconditional branch: loop no matter what
+  mov     w13, #10          // base 10
 
-  ; and finally print
-print:
-  mov	x0, #1           
-  adrp	x1, _res_str@PAGE
-  add	x1, x1, _res_str@PAGEOFF
-  mov	x2, x20          
-  mov	x16, #4   
-  svc	#0x80
-  ; also print new line
-  mov	x0, #1           
-  adrp  x1, _new_line@PAGE
-  add   x1, x1, _new_line@PAGEOFF
-  mov	x2, #1
-  mov	x16, #4   
-  svc	#0x80
+1:  // itoa loop
+  udiv    w14, w0, w13      
+  mul     w15, w14, w13     
+  sub     w16, w0, w15      
+  add     w16, w16, #48     
+  strb    w16, [x12], #1    
+  mov     w0, w14           
+  cbnz    w0, 1b            // branch back to label 1
+
+  adrp    x17, \_string@PAGE
+  add     x17, x17, \_string@PAGEOFF  
+  mov     x20, x12          
+  sub     x20, x20, x17     
+
+  sub     x12, x12, #1      
+
+2:  // reverse loop
+  cmp     x17, x12
+  b.ge    3f                // forward to label 3
+  ldrb    w19, [x17]        
+  ldrb    w21, [x12]        
+  strb    w21, [x17]        
+  strb    w19, [x12]
+  add     x17, x17, #1      
+  sub     x12, x12, #1
+  b       2b                // branch back to label 2
+
+3:  // print
+  mov     x0, #1           
+  adrp    x1, \_string@PAGE
+  add     x1, x1, \_string@PAGEOFF
+  mov     x2, x20          
+  mov     x16, #4   
+  svc     #0x80
 .endm
 
 .global _start
 _start:
-  
+  _PRINT_INT _first_n, _res_str
+  // add newline
+  mov   x0, #1
+  adrp  x1, _newline@PAGE
+  add   x1, x1, _newline@PAGEOFF
+  mov   x2, #1
+  mov   x16, #4
+  svc   #0x80
+
+  ; exit syscall: exit(status=0)
+  mov	x0, #0          
+  mov	x16, #1   
+  svc	#0x80
